7c82f535ae19d89451b531e7e525c4d1419a9b7b
SiberDesign <siberdesign2020@gmail.com>
2023-10-08 20:40:51 +0800

    [更新] VectorHelper增加公式 , Common 增加常見的 Sprite , Trail 的設定

--- Assets/SiberUtility/Tools/CommonHelper.cs
+++ Assets/SiberUtility/Tools/CommonHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace SiberUtility.Tools
+﻿using UnityEngine;
+
+namespace SiberUtility.Tools
 {
     /// <summary> 常見的工具 </summary>
     public static class CommonHelper
@@ -7,6 +9,39 @@
         public static void Toggle(ref bool boolean)
         {
             boolean = !boolean;
+        }
+    }
+
+    public static class TrailRendererHelper
+    {
+        /// <summary> TrailRenderer 設置啟用 </summary>
+        public static void SetEnable(this TrailRenderer trailRenderer, bool isEnable)
+        {
+            if (trailRenderer == null) return;
+            trailRenderer.enabled = isEnable;
+        }
+
+        public static void InitTrail(this TrailRenderer trailRenderer)
+        {
+            if (trailRenderer == null) return;
+            trailRenderer.Clear();
+            trailRenderer.time = 0f;
+        }
+
+        public static void SetTime(this TrailRenderer trailRenderer, float time)
+        {
+            if (trailRenderer == null) return;
+            trailRenderer.time = time;
+        }
+    }
+
+    public static class SpriteRendererHelper
+    {
+        /// <summary> SpriteRenderer 設置啟用 </summary>
+        public static void SetEnable(this SpriteRenderer spriteRenderer, bool isEnable)
+        {
+            if (spriteRenderer == null) return;
+            spriteRenderer.enabled = isEnable;
         }
     }
 }
--- Assets/SiberUtility/Tools/VectorHelper.cs
+++ Assets/SiberUtility/Tools/VectorHelper.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace SiberUtility.Tools
 {
@@ -9,25 +10,53 @@ namespace SiberUtility.Tools
     {
     #region ========== [Public Methods] ==========
 
-        /// <summary> 自製檢測 Vector2 距離 </summary>
+        /// <summary> AB 距離 (sqrMagnitude) </summary>
         /// <param name="pointA"> A點(this) </param>
         /// <param name="pointB"> B點 </param>
-        /// <returns></returns>
-        public static float Distance(this Vector2 pointA, Vector2 pointB)
+        public static float Distance_By_SqrMagnitude(this Vector2 pointA, Vector2 pointB)
         {
             var distance = (pointA - pointB).sqrMagnitude;
             return distance;
         }
 
-        /// <summary> 判斷 *兩點距離* 是否接近指定向量長度 </summary>
+        /// <summary> AB 距離 (Magnitude) </summary>
+        /// <param name="pointA"> A點(this) </param>
+        /// <param name="pointB"> B點 </param>
+        public static float Distance_By_Magnitude(this Vector2 pointA, Vector2 pointB)
+        {
+            var distance = (pointA - pointB).magnitude;
+            return distance;
+        }
+
+        /// <summary> AB 向量 </summary>
+        /// <param name="pointA"> A點(this) </param>
+        /// <param name="pointB"> B點 </param>
+        public static Vector2 Direction(this Vector2 pointA, Vector2 pointB)
+        {
+            var distance = (pointA - pointB).normalized;
+            return distance;
+        }
+
+        /// <summary> 是否在距離內 (SqrMagnitude) </summary>
         /// <param name="pointA"> A點 </param>
         /// <param name="pointB"> B點 </param>
         /// <param name="magnitude"> 依據的向量長度 </param>
-        /// <returns></returns>
         public static bool IsCloseThePoint(Vector2 pointA, Vector2 pointB, float magnitude)
         {
-            var distance = Distance(pointA, pointB);
-            return distance <= magnitude;
+            var distance = Distance_By_SqrMagnitude(pointA, pointB);
+            return distance <= magnitude * magnitude;
+        }
+
+        /// <summary> 滑鼠位置 (World) </summary>
+        /// <param name="camera"> 指定Camera </param>
+        public static Vector2 GetMousePosition(Camera camera = null)
+        {
+            if (camera == null) camera = Camera.main;
+            Assert.IsNotNull(camera, "camera == null");
+            var mousePos = Input.mousePosition;
+            mousePos.z = -camera.transform.position.z;
+            var result = camera.ScreenToWorldPoint(mousePos);
+            return result;
         }
 
     #endregion
