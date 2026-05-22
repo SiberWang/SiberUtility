d8114c019769e00a129e99108808df37af353083
Siber <siberdesign2020@gmail.com>
2026-05-22 17:19:31 +0800

    [更新] 停止使用 HierarchySectionHeader

--- Assets/SiberUtility/Editor/HierarchySectionHeader.cs
+++ Assets/SiberUtility/Editor/HierarchySectionHeader.cs
@@ -7,23 +7,24 @@ using UnityEngine;
 
 namespace SiberUtility.Editor
 {
-    [InitializeOnLoad]
-    public static class HierarchySectionHeader
-    {
-        static HierarchySectionHeader()
-        {
-            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
-        }
-
-        static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
-        {
-            var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
-
-            if (gameObject != null && gameObject.name.StartsWith("//", StringComparison.Ordinal))
-            {
-                EditorGUI.DrawRect(selectionRect, Color.black);
-                EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("/", "").ToUpperInvariant());
-            }
-        }
-    }
+    // 停用
+    // [InitializeOnLoad]
+    // public static class HierarchySectionHeader
+    // {
+    //     static HierarchySectionHeader()
+    //     {
+    //         EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+    //     }
+    //
+    //     static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+    //     {
+    //         var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+    //
+    //         if (gameObject != null && gameObject.name.StartsWith("//", StringComparison.Ordinal))
+    //         {
+    //             EditorGUI.DrawRect(selectionRect, Color.black);
+    //             EditorGUI.DropShadowLabel(selectionRect, gameObject.name.Replace("/", "").ToUpperInvariant());
+    //         }
+    //     }
+    // }
 }
